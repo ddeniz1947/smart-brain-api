07916668f1c6fdfe80f05c1701ac2e4d2afd22ba
Deniz Can DİNÇ <ddeniz1947@gmail.com>
2020-06-02 02:44:15 +0300

    update database

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-rigid-03398',
         user: 'postgres',
-        password: 'denizcan47',
+        password: '',
         database: 'smart-brain'
     }
 });
